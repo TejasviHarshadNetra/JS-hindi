ff81369d06961271ec4c3a26145884c4676fdec6
TejasviHarshadNetra <tejasvi.rananaware@gmail.com>
2025-05-29 07:00:56 +0000

    Investigation of variables.

--- .devcontainer/JSBasics/01_Variables.js
+++ .devcontainer/JSBasics/01_Variables.js
@@ -1,2 +1,10 @@
-const data="Tejasvi"
-console.log(data)
+const accountId=121223233;
+var test="Tejasvi"
+let data="Tejasvi Rananaware"
+
+//accountId1=2
+test="Netra"
+data="harshad"
+console.log(accountId);
+console.log(test);
+console.log(data);
