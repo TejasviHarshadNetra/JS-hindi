009e6b216c01483f206832975cb9a272320514a1
TejasviHarshadNetra <tejasvi.rananaware@gmail.com>
2025-05-29 07:26:09 +0000

    variabless intviest

--- .devcontainer/JSBasics/01_Variables.js
+++ .devcontainer/JSBasics/01_Variables.js
@@ -5,6 +5,11 @@ let data="Tejasvi Rananaware"
 //accountId1=2
 test="Netra"
 data="harshad"
+var emailId="tejasvi.bhosale944@gmail.com"
+let accountsdata;
+var accountssda;
 console.log(accountId);
 console.log(test);
 console.log(data);
+
+console.table([accountId,test,data,emailId,accountsdata,accountssda])
