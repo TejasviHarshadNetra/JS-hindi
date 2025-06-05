81687ab6877fbe400406f298f881fe9913d8389f
TejasviHarshadNetra <tejasvi.rananaware@gmail.com>
2025-06-05 13:05:55 +0000

    Datatypes investigations

---
+++ .devcontainer/JSBasics/Datatypes.js
@@ -0,0 +1,13 @@
+"use strict" //treat all JS code as newer version
+//alert("Hello") //we are usin gnode JS not browser
+let score="1"
+//console.log(typeof ((score)));
+console.log(typeof 123);           // "number"
+console.log(typeof "Hello");      // "string"
+console.log(typeof true);         // "boolean"
+console.log(typeof {});           // "object"
+console.log(typeof []);           // "object"
+console.log(typeof null);         // "object" ⚠️
+console.log(typeof undefined);    // "undefined"
+console.log(typeof function(){}); // "function"
+
